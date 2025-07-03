abf5a09630f487bb1fb643705b6b1a0b281f6331
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-07-03 22:20:42 +0700

    Create frankenphp-wp.sh

---
+++ frankenphp-wp.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#author : Anggarda Saputra Wijaya. anggarda.wijaya@outlook.com. github.com/anggardawjy
+#version : 0.1
+#about : FrankenPHP - Wordpress
+#
+#======== Update Repository & Package =======#
+apt update && upgrade -y
+apt install curl unzip htop -y
+#
+#
+#======== Install Go Languange ========#
+wget -c https://go.dev/dl/go1.24.4.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.24.4.linux-amd64.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+source ~/.bashrc
+#
+#
+#======== Install FrankenPHP ========#
+curl https://frankenphp.dev/install.sh | sh
+mv frankenphp /usr/local/bin/
+#
+#
+#======== Install Wordpress Lastest Update ========#
+mkdir /home/www
+cd /home/www
+wget https://wordpress.org/latest.zip
+unzip latest.zip
+mv wordpress html
+chown -R root:root /home/www
+#
+#
+#======== Install and Configure MYSQL Server ========#
+apt install mysql-server -y
+echo -n "Create Your Password Database : "
+read passwordmysql
+echo -n "Create Database For Wordpress : "
+read databasename
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$passwordmysql';"
+mysql -uroot -p$passwordmysql -e "GRANT ALL PRIVILEGES ON root.* TO 'root'@'localhost';"
+mysql -uroot -p$passwordmysql -e "CREATE DATABASE $databasename;"
+#
+#
+#======== Configure Caddyfile ========#
+mkdir /etc/caddy
+cp support/Caddyfile /etc/caddy/
+chown -R root:root /etc/caddy
+#
+#
+#======== Configure FrankenPHP Service ========#
+cp support/frankenphp.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable --now frankenphp
+systemctl restart frankenphp.service
+systemctl status frankenphp.service
+#
+#
